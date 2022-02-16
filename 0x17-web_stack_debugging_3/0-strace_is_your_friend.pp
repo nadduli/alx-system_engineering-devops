e705672f6e1011ff2e889a9d1baa0063378ed326
nadduli <naddulidaniel94@gmail.com>
2022-02-17 00:36:04 +0300

    fixed task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,12 @@
-# Fixes a wordpress site running on apache2
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# fix file extension typo
+exec { 'replace phpp with php':
+  command  => 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
+  provider => shell
 }
+
+#restart apache2
+exec { 'restart apache2 service':
+  command  => 'service apache2 restart',
+  provider => shell
+}
+
